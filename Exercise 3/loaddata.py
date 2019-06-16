59655ad41c9fddf7d0bff6ff89093384fb4c390a
LAPTOP-C82OUOKU\wenyu <guwenyu2007@126.com>
2019-06-16 10:57:47 +0200

    Reuse get ip sequence

--- Exercise 3/loaddata.py
+++ Exercise 3/loaddata.py
@@ -17,3 +17,21 @@ def load_data(path):
     df.drop(columns=['Src_IP_Addr:Port', 'Dst_IP_Addr:Port', '->'], inplace = True) 
     
     return df
+
+# Get sequential IP addresses connected to the host_ip
+def load_ip_sequence(df, host_ip):
+    
+    def filter_ip(row):
+        if row['src_ip'] == host_ip:
+            return row['dst_ip']
+        else:
+            return row['src_ip']
+    
+    # Filter out rows without host_ip
+    df = df[['src_ip', 'dst_ip']]
+    df = df[(df['src_ip'] == host_ip) | (df['dst_ip'] == host_ip)]
+    
+    # Filter out host_ip, store the other ip
+    df['connected'] = df.apply(filter_ip, axis=1)
+    
+    return df['connected'].tolist()
